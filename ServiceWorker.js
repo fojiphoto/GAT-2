f02a5d429bc07a204a5e71fd247f85965cabf031
Nawaz Ali <fojiphoto161@gmail.com>
2025-07-16 14:55:14 +0500

    Update ServiceWorker.js

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,18 +1,29 @@
-// This is the "Offline copy of pages" service worker
 
-const CACHE = "pwabuilder-offline";
+const CACHE_NAME = 'Guess The Quiz';
+const urlsToCache = [
+    '/',
+    '/index.html',
+    '/Build/f59037b19f972dc0b2652594798eb497.data.unityweb',
+    '/Build/f4ecbadad57c4ebc88341fe9350e277e.wasm.unityweb',
+    '/Build/c5e3ce1a0242c704135be4d866484562.framework.js.unityweb',
+    '/Build/9853637125e801e9aae48e78dbbdcfca.loader.js',
+    '/TemplateData/style.css',
+    '/icon-1920x1080.png',
+    '/icon-512x512.png'
+];
 
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
-
-self.addEventListener("message", (event) => {
-  if (event.data && event.data.type === "SKIP_WAITING") {
-    self.skipWaiting();
-  }
+self.addEventListener('install', function (event) {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(function (cache) {
+            return cache.addAll(urlsToCache);
+        })
+    );
 });
 
-workbox.routing.registerRoute(
-  new RegExp('/*'),
-  new workbox.strategies.StaleWhileRevalidate({
-    cacheName: CACHE
-  })
-);
+self.addEventListener('fetch', function (event) {
+    event.respondWith(
+        caches.match(event.request).then(function (response) {
+            return response || fetch(event.request);
+        })
+    );
+});
